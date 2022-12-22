af41d9182540f072929bce1698f2305761ca69b4
IlyaJa <718843i@mail.ru>
2022-12-22 17:03:39 +0900

    sum

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # otus-cpp-basic-attempt2
 test2
 test 1
+test Sum
--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int Sum(int a, int b){
+    return a+b;
+}
+
 int main(){
     std::cout<<"Hello Wordl!"<<std::endl;
+    std::cout<<Sum(4,8);
     return 0;
+
 }
