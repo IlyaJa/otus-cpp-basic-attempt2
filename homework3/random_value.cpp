782edf4cdbe59dea3ba976b1a6e54efcbbb9bb18
IlyaJa <718843i@mail.ru>
2023-02-06 14:15:28 +0900

    06.02.2023

--- homework3/random_value.cpp
+++ homework3/random_value.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include"random_value.h"
-//#include <cstdlib>
+#include <cstdlib>
 #include <ctime>
 
 int random_value() {
 
-	//const int max_value = 100;
+	const int max_value = 100;
 
-	std::srand(std::time(NULL)); // use current time as seed for random generator
+	std::srand(static_cast<unsigned int> (std::time(NULL))); // use current time as seed for random generator
 
-	const int random_value = std::rand() % 100+1;
+	const int random_value = std::rand() % max_value+1;
 
 	std::cout << random_value << std::endl;
 
